05ed562805048634a2382122a1a08e06370ad67e
Nourhakeem <nourhakem15@gmail.com>
2021-06-17 13:41:38 +0200

    edit currunt city weather

--- curWeather.js
+++ curWeather.js
@@ -5,6 +5,9 @@ window.onload = function getWeather() {
     let datevar = document.getElementById("datevar");
     let api = "https://api.openweathermap.org/data/2.5/weather";
     let apiKey = "e62c28be53f712ae84fa26a7c1ca4269";
+    let Curcityicon = document.getElementById("Curcityicon");
+    let windspeed = document.getElementById("windspeed");
+    let humidityval = document.getElementById("humidityval");
 
     location.innerHTML = "Locating...";
 
@@ -13,6 +16,8 @@ window.onload = function getWeather() {
     function success(position) {
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
+
+
 
         let urlw =
             api +
@@ -28,13 +33,19 @@ window.onload = function getWeather() {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                const icon = `https://openweathermap.org/img/wn/${
+       data.weather[0]["icon"]}@2x.png`;
                 let temp = data.main.temp;
                 temperature.innerHTML = Math.round(temp) + "° C";
                 console.log(data.dt)
                 datevar.innerHTML = new Date(data.dt * 1000).toLocaleString();
                 location.innerHTML =
-                    data.name;
-                description.innerHTML = data.weather[0].main;
+                    data.name + ", " + data.sys.country;
+                description.innerHTML = "Feels like " + Math.round(data.main.feels_like) + "° C, " + data.weather[0].description;
+                Curcityicon.src = icon;
+                Curcityicon.alt = data.weather[0].main;
+                windspeed.innerHTML = data.wind.speed + "m/s SSE"
+                humidityval.innerHTML = data.main.humidity + " %"
             });
     }
 
